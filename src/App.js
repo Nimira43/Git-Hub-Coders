919a6def5f6a1d933bcc2b17c047aec32c421da4
Nimira43 <n.rabone72@aol.com>
2024-09-22 23:55:42 +0100

    Starting on Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-const App = () => {
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+import Navbar from './components/layout/Navbar'
+
+function App() {
   return (
-    <div>
-      <h1>Git Hub Coders</h1>
-    </div>
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
+        <main>
+          Content
+        </main>
+      </div>
+    </Router>
   )
 }
 
