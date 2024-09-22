be232648fb1065125e5d26dce7d734b1c15309a8
Nimira43 <n.rabone72@aol.com>
2024-09-23 00:13:35 +0100

    Some issues here.

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,14 +1,22 @@
-import { FaGithub } from 'react-icons/fa'
-import { Link } from 'react-router-dom'
+// import { FaGithub } from 'react-icons/fa'
+// import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 
-function Navbar() {
+function Navbar({ title }) {
   return (
-    <div>
+    <nav className='navbar mb-12 shadow-lg bg-neutral text-neutral-content'>
       Navbar
-    </div>
+    </nav>
   )
+}
+
+Navbar.defaultProps = {
+  title: 'Github Coders'
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string,
 }
 
 export default Navbar
